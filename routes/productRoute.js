1ea0a0f4ba813e8ac2507ff8afed0e89f7bfb0e9
VeronicaVassallo <vvassallo94@gmail.com>
2024-02-13 18:22:57 +0100

    wip cloudinary

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,69 @@
 const express = require("express");
 const productModel = require("../models/productModel");
 const productRouter = express.Router();
+
+//cloudinary
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+require("dotenv").config;
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const cloudStorage = new CloudinaryStorage({
+	cloudinary: cloudinary,
+	params: {
+		folder: "public",
+		format: async (req, file) => "jpg",
+		public_id: (req, file) => file.name,
+	},
+});
+const cloudUpload = multer({ storage: cloudStorage });
+
+productRouter.post(
+	"/post/cloudUpload",
+	cloudUpload.single("img"),
+	async (req, res) => {
+		try {
+			res.status(200).json({ img: req.file.path });
+		} catch (e) {
+			res.status(500).send({
+				statusCode: 500,
+				message: "Internal server error" + error,
+				error,
+			});
+		}
+	}
+);
+/*POST // to do: CONTINUA CLOUDINARI*/
+productRouter.post("/product/create", async (req, res) => {
+	const newProduct = new productModel({
+		productName: req.body.productName,
+		ingredients: req.body.ingredients,
+		price: Number(req.body.price),
+		img: req.body.img,
+		typology: req.body.typology,
+	});
+
+	try {
+		const postRequest = newProduct.save();
+
+		res.status(201).send({
+			statusCode: 201,
+			message: "Product created",
+		});
+	} catch (error) {
+		res.status(500).send({
+			statusCode: 500,
+			message: "internal server error",
+		});
+	}
+});
+
 /*GET */
 productRouter.get("/product", async (req, res) => {
 	try {
@@ -40,28 +103,5 @@ productRouter.get("/product/:typology", async (req, res) => {
 		});
 	}
 });
-/*POST */
-productRouter.post("/product/create", async (req, res) => {
-	const newProduct = new productModel({
-		productName: req.body.productName,
-		ingredients: req.body.ingredients,
-		price: Number(req.body.price),
-		img: req.body.img,
-		typology: req.body.typology,
-	});
 
-	try {
-		const postRequest = newProduct.save();
-
-		res.status(201).send({
-			statusCode: 201,
-			message: "Product created",
-		});
-	} catch (error) {
-		res.status(500).send({
-			statusCode: 500,
-			message: "internal server error",
-		});
-	}
-});
 module.exports = productRouter;
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const productRouter = require("../laGrandeGiaraBE/routes/productRoute");
+const path = require("path");
 
 require("dotenv").config();
 const cors = require("cors");
 const PORT = 5050;
 
 const app = express();
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+//middleware
 app.use(cors());
 app.use(express.json());
+//routes
 app.use("/", productRouter);
 
 mongoose.connect(process.env.MONGODB_URL, {});
