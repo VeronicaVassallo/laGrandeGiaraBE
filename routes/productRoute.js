84c6e979575f89a72e51c20e835931d6717db39a
VeronicaVassallo <vvassallo94@gmail.com>
2024-02-14 18:31:15 +0100

    delete product + wip put product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -104,4 +104,51 @@ productRouter.get("/product/:typology", async (req, res) => {
 	}
 });
 
+/*DELETE*/
+productRouter.delete("/product/delete/:idProduct", async (req, res) => {
+	const { idProduct } = req.params;
+
+	try {
+		const productToDelete = await productModel.findByIdAndDelete({
+			_id: idProduct,
+		});
+		res.status(200).send({
+			statusCode: 200,
+			message: `Product with _id: ${idProduct} is deleted`,
+		});
+	} catch (error) {
+		res.status(500).send({
+			statusCode: 500,
+			message: `Internal server Error: ${error}`,
+			error,
+		});
+	}
+});
+
+/*PUT */
+productRouter.put("/product/modify/:idProduct", async (req, res) => {
+	const { idProduct } = req.params;
+	try {
+		const productModify = await productModel.findByIdAndUpdate(idProduct, {
+			productName: req.body.productName,
+			ingredients: req.body.ingredients,
+			price: Number(req.body.price),
+			img: req.body.img,
+			typology: req.body.typology,
+		});
+
+		res.status(200).send({
+			statusCode: 200,
+			message: `Product with id: ${idProduct} is modified`,
+			productModify,
+		});
+	} catch (error) {
+		res.status(500).send({
+			statusCode: 500,
+			message: `Internal server error: ${error}`,
+			error,
+		});
+	}
+});
+
 module.exports = productRouter;
